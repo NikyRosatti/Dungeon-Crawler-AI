18dc20e4a1a5e2083a488cf34e76793f70eea3ec
eg-v <136094198+eg-v@users.noreply.github.com>
2024-08-30 20:17:40 -0300

    Add flask app

---
+++ flask_server/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import threading
+import time
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+current_position = (0, 0)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('position_update')
+def handle_position_update(data):
+    global current_position
+    current_position = data['position']
+    emit('position_update', {'position': current_position}, broadcast=True)
+
+def send_position():
+    while True:
+        socketio.emit('position_update', {'position': current_position})
+        time.sleep(0.2)
+
+thread = threading.Thread(target=send_position)
+thread.start()
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)

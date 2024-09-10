60f29d1da8fbf871671415dfe59bf47413da192e
NikyRosatti <nicollerosatti@gmail.com>
2024-09-10 16:05:24 -0300

    Add Register and Map

--- front/app.py
+++ front/app.py
@@ -94,5 +94,10 @@ def register():
 
     return render_template('register.html')
 
+@app.route('/map')
+def map():
+    return render_template('map.html')
+   
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)

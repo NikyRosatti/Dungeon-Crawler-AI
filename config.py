049e84720342143c16b65a0ec00d8c9fc496cfbd
Joaquin Tissera <phoenix_css@hotmail.com>
2024-09-17 17:47:22 -0300

    Config: Revert database creation

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysecretkey'
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app', 'instance', 'dataBase.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///dataBase.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

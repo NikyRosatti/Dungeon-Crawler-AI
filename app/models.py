f64c301eb5cb3ce334a9ceff8dd16fe8c995404c
Joaquin Tissera <phoenix_css@hotmail.com>
2024-09-21 03:47:57 -0300

    Database models and creador.js
    Properly merge with main branch

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 from datetime import datetime
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
@@ -9,14 +10,24 @@ class User(db.Model):
     completed_dungeons = db.Column(db.Integer, nullable=False, default=0)
     avatar = db.Column(db.String(200), nullable=True)
     joined_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    
+
     def __repr__(self):
-        return f'<User {self.username}>'
-    
+        return f"<User {self.username}>"
+
+
 class MazeBd(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    grid = db.Column(db.JSON, nullable = False)
-    
+    grid = db.Column(db.JSON, nullable=False)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("user.id"), nullable=False
+    )  # FK a User
+    created_at = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow
+    )  # Fecha de creación
+    maze_size = db.Column(db.Integer, nullable=False)
+
+    # Relación con el modelo User
+    user = db.relationship("User", backref="mazes", lazy=True)
+
     def __repr__(self):
-        return f'<MazeBd {self.grid}>'
-    
+        return f"<MazeBd {self.grid}>"

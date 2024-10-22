69013043b6859bac3c097d6b31fefe4371d8d02a
nalovaleno <valentinhormiga@hotmail.com>
2024-10-21 21:24:57 -0300

    cambios en la configuracion de la base de datos para poder usarla en el ejecutable

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 import os
-
+import sys
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysecretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///dataBase.db'
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    if getattr(sys, 'frozen', False):
+        # Si está en un ejecutable, asegura que apunte al directorio correcto
+        SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(sys._MEIPASS, "instance", "dataBase.db")}'
+    else:
+        # Si no, apunta al directorio del proyecto
+        SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "instance", "dataBase.db")}'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(Config):
